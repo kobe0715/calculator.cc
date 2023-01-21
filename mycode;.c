02c5358cd05a3b81336234502ec788a7fc5e50e6
kobe0715 <123227244+kobe0715@users.noreply.github.com>
2023-01-21 14:28:02 +0800

    mycode.c
    its calculator allows user to compute multiplication addition subtraction division with right operator

---
+++ mycode;.c
@@ -0,0 +1,35 @@
+calculator in c
+
+include<stdio.h>
+
+int 1num;
+char operator;
+int 2num;
+int main (void){
+
+printf("hello welcome to my calculator:\n");
+printf("please enter the first number:");
+scanf("%d",&1num)
+printf("please enter the operator r:");
+scanf(" %c",&operator)
+printf("please enter the first number:");
+scanf("%d",&2num)
+
+if (operator == '+')
+prinf("THE ANSWER IS: %d ", 1num+2num);
+else if (operator == '-')
+prinf("THE ANSWER IS: %d ",1num-2num);
+else if (operator == '*')
+prinf("THE ANSWER IS: %d ",1num*2num);
+else if (operator == '/')
+prinf("THE ANSWER IS: %d ",1numL\2num);
+else
+printf("i cant recognized your operator......:");
+
+
+
+
+
+
+
+}
